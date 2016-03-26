8f3bcd321a6b2dcb82c0e2ce2380aa7d0d946212
nikhilsingh291 <nikhilsingh291@gmail.com>
2016-03-26 13:27:09 +0530

    first commit

---
+++ src/WebImgExtractor.java
@@ -0,0 +1,85 @@
+/** 
+ * Copyright (C) 2016 Nikhil Singh <nikhilsingh291@gmail.com>
+ * WebImgExtractor is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    WebImgExtractor is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with WebImgExtractor.  If not, see <http://www.gnu.org/licenses/>.
+    * */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import javax.swing.text.html.HTML;
+import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.html.parser.ParserDelegator;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import javax.imageio.ImageIO;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.html.HTMLDocument;
+
+public class WebImgExtractor {
+    public static void main(String args[]) throws Exception {
+
+        String saveLoc = args[1];
+        String webUrl = "http://"+args[0];
+        URL url = new URL(webUrl);
+        URLConnection connection = url.openConnection();
+        InputStream is = connection.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+
+        HTMLEditorKit htmlKit = new HTMLEditorKit();
+        HTMLDocument htmlDoc = (HTMLDocument) htmlKit.createDefaultDocument();
+        HTMLEditorKit.Parser parser = new ParserDelegator();
+        HTMLEditorKit.ParserCallback callback = htmlDoc.getReader(0);
+        parser.parse(br, callback, true);
+
+        for (HTMLDocument.Iterator iterator = htmlDoc.getIterator(HTML.Tag.IMG); iterator.isValid(); iterator.next()) {
+            AttributeSet attributes = iterator.getAttributes();
+            String imgSrc = (String) attributes.getAttribute(HTML.Attribute.SRC);
+
+            if (imgSrc != null && (imgSrc.endsWith(".jpg") || (imgSrc.endsWith(".png")) || (imgSrc.endsWith(".jpeg")) || (imgSrc.endsWith(".bmp")) || (imgSrc.endsWith(".ico")))) {
+                try {
+                    downloadImage(webUrl, imgSrc,saveLoc);
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        }
+    }
+    private static void downloadImage(String url, String imgSrc,String savdir) throws IOException {
+        BufferedImage image = null;
+        try {
+            if (!(imgSrc.startsWith("http"))) {
+                url = url + imgSrc;
+            } else {
+                url = imgSrc;
+            }
+            imgSrc = imgSrc.substring(imgSrc.lastIndexOf("/") + 1);
+            String imageFormat = null;
+            imageFormat = imgSrc.substring(imgSrc.lastIndexOf(".") + 1);
+            String imgPath = null;
+            imgPath = savdir + imgSrc + "";
+            URL imageUrl = new URL(url);
+            image = ImageIO.read(imageUrl);
+            if (image != null) {
+                File file = new File(imgPath);
+                ImageIO.write(image, imageFormat, file);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
